425d0b6c77a9bc96a8b84b91448d338970768f46
TomEBriggs <60936935+TomEBriggs@users.noreply.github.com>
2021-04-19 23:25:05 +0100

    Delete Axivity directory

--- Axivity/target/classes/static/axivity.js
+++
@@ -1,8 +0,0 @@
-function onClickMenu(){
-    document.getElementById("hamburger").classList.toggle("change");
-    document.getElementById("nav-bar").classList.toggle("nav-open");
-}
-function visuals(){
-    document.getElementById("visualsList").classList.toggle("change-visible");
-    document.getElementById("visualsArrow").classList.toggle("rotate-arrow");
-}
