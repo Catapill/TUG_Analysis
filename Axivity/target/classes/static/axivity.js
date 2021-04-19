9d158e643f70e0c02fc425db581b8a333163ff6f
TomEBriggs <60936935+TomEBriggs@users.noreply.github.com>
2021-04-19 23:26:24 +0100

    Web Interface
    Online platform for viewing the project

---
+++ Axivity/target/classes/static/axivity.js
@@ -0,0 +1,8 @@
+function onClickMenu(){
+    document.getElementById("hamburger").classList.toggle("change");
+    document.getElementById("nav-bar").classList.toggle("nav-open");
+}
+function visuals(){
+    document.getElementById("visualsList").classList.toggle("change-visible");
+    document.getElementById("visualsArrow").classList.toggle("rotate-arrow");
+}
